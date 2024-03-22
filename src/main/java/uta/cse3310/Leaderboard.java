98f17e7639e81a6f1640cc5b6ffef34dd6698208
Ayman Azahar <ara4840@mavs.uta.edu>
2024-03-21 23:05:31 -0500

    Leaderboard class

---
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -0,0 +1,28 @@
+package main.java.uta.cse3310;
+
+import java.util.*; 
+
+public class Leaderboard{
+
+        
+
+
+        List<PLayerType> players; 
+        GameStats finalGameStats; 
+          
+        public Leaderboard(){
+        players = new ArrayList<>();
+        finalGameStats = new GameStats();
+        }
+
+
+        public void display(){}
+
+        public void reset(){}
+
+     }
+
+        
+
+
+}
