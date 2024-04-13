b652d0aae75b80cbbbe70afcafc0ca452e943ca3
Anthony Fierro <anthonyfierro@anthonys-air.attlocal.net>
2024-04-12 22:19:00 -0500

    added getters and setters for variables

--- src/main/java/uta/cse3310/UserEvent.java
+++ src/main/java/uta/cse3310/UserEvent.java
@@ -5,4 +5,19 @@ public class UserEvent {
     int GameId; // the game ID on the server
     PlayerType Players; // players in lobby
     int Button; // button number from 0 to 2499
+
+    public int getGameId() {
+        return GameId;
+    }
+
+    //getter for players
+    public PlayerType getPlayers() {
+        return Players;
+    }
+
+    //getter for button
+    public int getButton() {
+        return Button;
+    }
+
 }
