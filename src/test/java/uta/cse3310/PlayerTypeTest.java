4174bf6ef3e4f2e627de61042d8c28701dea3741
Anthony Fierro <anthonyfierro@anthonys-air.attlocal.net>
2024-04-14 21:12:12 -0500

    correct error in testing

--- src/test/java/uta/cse3310/PlayerTypeTest.java
+++ src/test/java/uta/cse3310/PlayerTypeTest.java
@@ -10,14 +10,22 @@ import com.google.gson.GsonBuilder;
 
 public class PlayerTypeTest {
 
+    @Test(expected = IllegalArgumentException.class)
+    public void checkNullNickname() {
+        new PlayerType(null, "Green", PlayerType.Status.Waiting);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkNullColor() {
+        new PlayerType("Anthony", null, PlayerType.Status.Waiting);
+    }
+
     @Test
-    public void CheckNickAndColor(){
-        try{
-            new PlayerType(null, "Green", PlayerType.Status.Waiting);
-            fail("Expected IllegalArgumentException for null nickname");
-        }catch(IllegalArgumentException e){
-            assertEquals("Nickname cannot be null.", e.getMessage());
-        }
+    public void checkValidNicknameAndColor() {
+        PlayerType player = new PlayerType("Anthony", "Green", PlayerType.Status.Waiting);
+        assertEquals("Anthony", player.getNickname());
+        assertEquals("Green", player.getColor());
+        assertEquals(PlayerType.Status.Waiting, player.getStatus());
     }
     
 }
